4484343bf58aba86cda95a08f2c142870a950289
Sarthak-Malla <spm9513@nyu.edu>
2025-04-05 22:43:58 +0400

    deploying to ec2: debug backend api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080/api";
-console.log("env API_URL", process.env.REACT_APP_API_URL);
 
 // Job types and interfaces
 export interface Job {
